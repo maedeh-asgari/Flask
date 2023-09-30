7ce157408f8102b6ce20a6ad7f18c9e94e89047e
maedeh-asgari <143606449+maedeh-asgari@users.noreply.github.com>
2023-09-30 11:41:29 +0330

    Update PersonFlaskTask.py

--- PersonFlaskTask.py
+++ PersonFlaskTask.py
@@ -1,40 +1,71 @@
-from flask import Flask, session, render_template
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///person.db"
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///persons.db'
 db = SQLAlchemy(app)
 
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150))
-    lastname = db.Column(db.String(150))
-    age = db.Column(db.Integer)
-    email = db.Column(db.String(500))
-
-    def __repr__(self):
-        return f'{self.name}-{self.lastname}-{self.age}-{self.email}'
-
-
-@app.route('/')
-def main():
-    p1 = Person(name='Maedeh', lastname='Asgari', age=23, email='maedeh@gmail.com')
-    db.session.add(p1)
-    db.session.commit()
-    p2 = Person(name='Helia', lastname='Ahmadi', age=18, email='helia@gmail.com')
-    db.session.add(p2)
-    db.session.commit()
-    persons = Person.query.all()
-    return render_template('base.html', persons=persons)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    mobile = db.Column(db.String(20))
+    birthdate = db.Column(db.Date, nullable=False)
 
 
 @app.before_request
-def dbconnection():
+def database():
     db.create_all()
-    print("Database was created")
+
+
+@app.route('/persons', methods=['GET'])
+def get_persons():
+    persons = Person.query.all()
+    person_list = [{"id": person.id, "first_name": person.first_name,
+                    "last_name": person.last_name, "mobile": person.mobile,
+                    "birthdate": person.birthdate.strftime('%Y-%m-%d')} for person in persons]
+    return jsonify({"persons": person_list})
+
+
+@app.route('/persons', methods=['POST'])
+def add_person():
+    data = request.get_json()
+    new_person = Person(
+        first_name=data['first_name'],
+        last_name=data['last_name'],
+        mobile=data['mobile'],
+        birthdate=datetime.strptime(data['birthdate'], '%Y-%m-%d')
+    )
+    db.session.add(new_person)
+    db.session.commit()
+    return jsonify({'message': 'person created successfully'})
+
+
+@app.route('/persons/<int:id>', methods=['PUT'])
+def update_person(id):
+    person = Person.query.get(id)
+    if person is None:
+        return jsonify({'message': 'person not found'}), 404
+
+    data = request.get_json()
+    person.first_name = data['first_name']
+    person.last_name = data['last_name']
+    person.mobile = data['mobile']
+    person.birthdate = datetime.strptime(data['birthdate'], '%Y-%m-%d')
+    db.session.commit()
+    return jsonify({'message': 'person updated successfully'})
+
+
+@app.route('/persons/<int:id>', methods=['DELETE'])
+def delete_person(id):
+    person = Person.query.get(id)
+    if person is None:
+        return jsonify({'message': 'person not found'}), 404
+    db.session.delete(person)
+    db.session.commit()
+    return jsonify({'message': 'person deleted successfully'})
 
 
 if __name__ == '__main__':
